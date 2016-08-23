fe0c9422dab2eaebe983c1cee1fbd6680295962d
Brian Caruso <carusobp@gmail.com>
2016-08-23 16:52:20 -0700

    Add XML documentation for public methods and interfaces

--- src/Invio.Extensions.DependencyInjection/IFactory.cs
+++ src/Invio.Extensions.DependencyInjection/IFactory.cs
@@ -2,9 +2,22 @@ using System;
 
 namespace Invio.Extensions.DependencyInjection {
 
-    public interface IFactory<T> {
+    /// <summary>
+    /// Implementation of factory pattern to provide services for use
+    /// with a implementation of <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <typeparam name="TService">The type of the service the factory provides.</typeparam>
+    public interface IFactory<TService> {
 
-        T Provide();
+        /// <summary>
+        /// Provides an object of the type <typeparamref name="TService"/>. It will
+        /// run based upon the lifetime specified for <typeparamref name="TService"/>
+        /// in the overarching <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <returns>
+        /// An instance of the <typeparamref name="TService"/> defined for the factory.
+        /// </returns>
+        TService Provide();
 
     }
 
--- src/Invio.Extensions.DependencyInjection/ServiceCollectionFactoryExtensions.cs
+++ src/Invio.Extensions.DependencyInjection/ServiceCollectionFactoryExtensions.cs
@@ -5,8 +5,25 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Invio.Extensions.DependencyInjection {
 
+    /// <summary>
+    /// Extension methods for adding services to an <see cref="IServiceCollection" />.
+    /// </summary>
     public static class ServiceCollectionFactoryExtensions {
 
+        /// <summary>
+        /// Adds a transient service of the type specified in <typeparamref name="TService"/> that
+        /// will be hydrated via a factory specified in <typeparamref name="TServiceFactory"/>
+        /// to the specified <see cref="IServiceCollection" />. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TServiceFactory">The type of the factory to use.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
+        /// <seealso cref="ServiceLifetime.Transient" />
         public static IServiceCollection AddTransientWithFactory<TService, TServiceFactory>(
             this IServiceCollection collection)
                 where TService : class
@@ -15,6 +32,20 @@ namespace Invio.Extensions.DependencyInjection {
             return collection.AddTransientWithFactory(typeof(TService), typeof(TServiceFactory));
         }
 
+        /// <summary>
+        /// Adds a transient service of the type specified in <paramref name="serviceType"/> that
+        /// will be hydrated via a factory specified in <paramref name="factoryType"/>
+        /// to the specified <see cref="IServiceCollection" />. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="serviceType">The type of the service to register.</param>
+        /// <param name="factoryType">The type of the factory to use.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
+        /// <seealso cref="ServiceLifetime.Transient" />
         public static IServiceCollection AddTransientWithFactory(
             this IServiceCollection collection,
             Type serviceType,
@@ -23,6 +54,20 @@ namespace Invio.Extensions.DependencyInjection {
             return collection.AddWithFactory(serviceType, factoryType, ServiceLifetime.Transient);
         }
 
+        /// <summary>
+        /// Adds a scoped service of the type specified in <typeparamref name="TService"/> that
+        /// will be hydrated via a factory specified in <typeparamref name="TServiceFactory"/>
+        /// to the specified <see cref="IServiceCollection" />. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TServiceFactory">The type of the factory to use.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
+        /// <seealso cref="ServiceLifetime.Scoped" />
         public static IServiceCollection AddScopedWithFactory<TService, TServiceFactory>(
             this IServiceCollection collection)
                 where TService : class
@@ -31,6 +76,20 @@ namespace Invio.Extensions.DependencyInjection {
             return collection.AddScopedWithFactory(typeof(TService), typeof(TServiceFactory));
         }
 
+        /// <summary>
+        /// Adds a scoped service of the type specified in <paramref name="serviceType"/> that
+        /// will be hydrated via a factory specified in <paramref name="factoryType"/>
+        /// to the specified <see cref="IServiceCollection" />. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="serviceType">The type of the service to register.</param>
+        /// <param name="factoryType">The type of the factory to use.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
+        /// <seealso cref="ServiceLifetime.Scoped" />
         public static IServiceCollection AddScopedWithFactory(
             this IServiceCollection collection,
             Type serviceType,
@@ -39,6 +98,20 @@ namespace Invio.Extensions.DependencyInjection {
             return collection.AddWithFactory(serviceType, factoryType, ServiceLifetime.Scoped);
         }
 
+        /// <summary>
+        /// Adds a singleton service of the type specified in <typeparamref name="TService"/> that
+        /// will be hydrated via a factory specified in <typeparamref name="TServiceFactory"/>
+        /// to the specified <see cref="IServiceCollection" />. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TServiceFactory">The type of the factory to use.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
+        /// <seealso cref="ServiceLifetime.Singleton" />
         public static IServiceCollection AddSingletonWithFactory<TService, TServiceFactory>(
             this IServiceCollection collection)
                 where TService : class
@@ -47,6 +120,20 @@ namespace Invio.Extensions.DependencyInjection {
             return collection.AddSingletonWithFactory(typeof(TService), typeof(TServiceFactory));
         }
 
+        /// <summary>
+        /// Adds a singleton service of the type specified in <paramref name="serviceType"/> that
+        /// will be hydrated via a factory specified in <paramref name="factoryType"/>
+        /// to the specified <see cref="IServiceCollection" />. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="serviceType">The type of the service to register.</param>
+        /// <param name="factoryType">The type of the factory to use.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
+        /// <seealso cref="ServiceLifetime.Singleton" />
         public static IServiceCollection AddSingletonWithFactory(
             this IServiceCollection collection,
             Type serviceType,
@@ -55,6 +142,21 @@ namespace Invio.Extensions.DependencyInjection {
             return collection.AddWithFactory(serviceType, factoryType, ServiceLifetime.Singleton);
         }
 
+        /// <summary>
+        /// Adds a service of the type specified in <paramref name="serviceType"/> that
+        /// will be hydrated via a factory specified in <paramref name="factoryType"/>
+        /// to the specified <see cref="IServiceCollection" />. Its lifetime is controlled
+        /// by the value specified via <paramref name="lifetime"/>. If the factory is not yet added
+        /// to the <see cref="IServiceCollection" />, it will also be added as well.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="serviceType">The type of the service to register.</param>
+        /// <param name="factoryType">The type of the factory to use.</param>
+        /// <param name="lifetime">The lifetime of the service before the factory is reused.</param>
+        /// <returns>
+        /// A reference to this <see cref="IServiceCollection"/> instance after the operation has
+        /// completed.
+        /// </returns>
         public static IServiceCollection AddWithFactory(
             this IServiceCollection collection,
             Type serviceType,
